cd794397ff809c88433335fc2188608c5d459f9e
QUINN STOBBS <quinn@atlasagency.com.au>
2018-05-20 17:49:54 +1000

    uploading the next assignment and redux lectures

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,35 +5,35 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+    // state = {
+    //     counter: 0
+    // }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
+    // counterChangedHandler = ( action, value ) => {
+    //     switch ( action ) {
+    //         case 'inc':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    //             break;
+    //         case 'dec':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+    //             break;
+    //         case 'add':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+    //             break;
+    //         case 'sub':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+    //             break;
+    //     }
+    // }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctrState} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -48,5 +48,23 @@ const mapStateToProps = state => {
     };
 }
 
+// this will call dispatch on the store
+// this will dispatch everything
+// THIS IS RETURNING AN OBJECT - I HAVE TO RETURN AN OBJECT
+const mapDispatchToProps = dispatch => {
+
+    // this will be available through disk property
+    // have to have the type which is always in caps
+    // --- ACTION ----- //
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD', val: 10}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val: 15})
+        // onAddCounter
+    };
+};
+
+
 // connect is a higher order function which returns a function 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,47 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action ) => {
+
+    switch (action.type){
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter -1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.val
+            }
+    }
+
+    // if(action.type === 'INCREMENT'){
+    //     return {
+    //         // below is an immutable state
+    //         // ...state
+    //         counter: state.counter + 1
+    //     }
+    // } else if (action.type === 'DECREMENT'){
+    //     return {
+    //         counter: state.counter -1
+    //     }
+    // } else if ( action.type === 'ADD'){
+    //     return { 
+    //         counter: state.counter + action.val
+    //     }
+    // } else if (action.type === 'SUBTRACT'){
+    //     return {
+    //         counter: state.counter - action.val
+    //     }
+    // }
+
+    // return the current state to not break the application
     return state
 }
 
